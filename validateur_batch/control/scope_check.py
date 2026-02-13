27c67c4b63baf5fe25444cfd1eb5bdbb708c1333
Pierre-Olivier Grégoire <pierre-olivier.gregoire@phast.fr>
2026-02-13 10:09:01 +0100

    Format improvement

--- validateur_batch/control/scope_check.py
+++ validateur_batch/control/scope_check.py
@@ -1,38 +1,59 @@
 from validateur_batch.scope import Scope
 import pandas as pd
 
-def check_scope_completeness(scope: Scope, preview: pd.DataFrame) -> pd.DataFrame:
+
+def check_scope_completeness(
+    scope: Scope, preview: pd.DataFrame
+) -> pd.DataFrame:
     """
-    Vérifie que tous les concepts présents dans le périmètre d'analyse sont présents dans le DataFrame de prévisualisation.
+    Vérifie que tous les concepts présents dans le périmètre d'analyse
+    sont présents dans le DataFrame de prévisualisation.
 
     Args:
         scope (Scope): Objet Scope contenant le périmètre d'analyse.
-        preview (pd.DataFrame): DataFrame de prévisualisation contenant les concepts à vérifier, avec une colonne "conceptId".
+        preview (pd.DataFrame): DataFrame de prévisualisation contenant
+            les concepts à vérifier, avec une colonne "conceptId".
 
     Returns:
-        pd.DataFrame: DataFrame du scope enrichi d'une colonne "in_delivery" indiquant si chaque concept du périmètre d'analyse est présent dans le DataFrame de prévisualisation.
+        pd.DataFrame: DataFrame du scope enrichi d'une colonne "in_delivery" indiquant
+            si chaque concept du périmètre d'analyse est présent dans le DataFrame
+            de prévisualisation.
     """
     if scope is None:
-        raise ValueError("Le périmètre d'analyse doit être défini pour effectuer la vérification de complétude.")
-    
+        raise ValueError(
+            "Le périmètre d'analyse doit être défini pour " +
+            "effectuer la vérification de complétude."
+        )
+
     scope_df = scope.full_scope_df.copy()
-    scope_df["in_delivery"] = scope_df["conceptId"].isin(preview["conceptId"])
+    preview_concept_ids = set(preview["conceptId"])
+    scope_df["in_delivery"] = scope_df["conceptId"].isin(preview_concept_ids)
     return scope_df
 
-def check_scope_exclusivity(scope: Scope, preview: pd.DataFrame) -> pd.DataFrame:
+
+def check_scope_exclusivity(
+    scope: Scope, preview: pd.DataFrame
+) -> pd.DataFrame:
     """
-    Vérifie que tous les concepts présents dans le DataFrame de prévisualisation sont présents dans le périmètre d'analyse.
+    Vérifie que tous les concepts présents dans le DataFrame de prévisualisation
+    sont présents dans le périmètre d'analyse.
 
     Args:
         scope (Scope): Objet Scope contenant le périmètre d'analyse.
-        preview (pd.DataFrame): DataFrame de prévisualisation contenant les concepts à vérifier, avec une colonne "conceptId".
+        preview (pd.DataFrame): DataFrame de prévisualisation contenant
+            les concepts à vérifier, avec une colonne "conceptId".
 
     Returns:
-        pd.DataFrame: DataFrame de prévisualisation enrichi d'une colonne "in_scope" indiquant si chaque concept de la prévisualisation est présent dans le périmètre d'analyse.
+        pd.DataFrame: DataFrame de prévisualisation enrichi d'une colonne "in_scope"
+            indiquant si chaque concept de la prévisualisation est présent 
+            dans le périmètre d'analyse.
     """
     if scope is None:
-        raise ValueError("Le périmètre d'analyse doit être défini pour effectuer la vérification d'exclusivité.")
-    
+        raise ValueError(
+            "Le périmètre d'analyse doit être défini "
+            + "pour effectuer la vérification d'exclusivité."
+        )
+
     scope_concept_ids = set(scope.full_scope_df["conceptId"])
     preview = preview.copy()
     preview["in_scope"] = preview["conceptId"].isin(scope_concept_ids)
